8e92b87b2f41f27a0386f46920fa356b0ee79f39
Ong Heng Kiat <onghengkiat105@gmail.com>
2021-10-25 17:21:48 +0800

    Dynamic API url based on development or production

--- frontend/src/constants/API.js
+++ frontend/src/constants/API.js
@@ -1,1 +1,1 @@
-export const URL_PREFIX = "http://127.0.0.1:8000/"
+export const URL_PREFIX = (process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://127.0.0.1:8000": "https://currency-exchange-rate-be.herokuapp.com/";
