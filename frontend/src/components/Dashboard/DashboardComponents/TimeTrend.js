6f65aa9f62d7e2c4b2cedefa0875b98f12d89976
Ong Heng Kiat <onghengkiat105@gmail.com>
2021-12-30 14:45:49 +0800

    Modify to color blind friendly palette

--- frontend/src/components/Dashboard/DashboardComponents/TimeTrend.js
+++ frontend/src/components/Dashboard/DashboardComponents/TimeTrend.js
@@ -16,7 +16,7 @@ export default function TimeTrend({ data, chartTitle }){
     )
   }
 
-  const colors = ['blue', 'green', 'red'];
+  const colors = ['#0071b2', '#e69f00', '#d55c00'];
   const legends = ["From MYR", "GDP Growth Rate, %", "CPI"]
   
   const option = {
